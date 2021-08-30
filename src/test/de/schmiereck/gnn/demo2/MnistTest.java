cbb2ff12d5c95eebe57e4d662e4429fbbdda9f41
schmiereck <thomas@schmiereck.de>
2021-08-30 14:31:03 +0200

    Add Mnist.

---
+++ src/test/de/schmiereck/gnn/demo2/MnistTest.java
@@ -0,0 +1,39 @@
+package de.schmiereck.gnn.demo2;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Objects;
+
+import org.junit.jupiter.api.Test;
+
+import de.schmiereck.gnn.Main;
+import de.schmiereck.gnn.MnistUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MnistTest {
+
+    @Test
+    public void testReadMnistData() {
+        // Arrange
+        final String fileName = "./mnist_train.csv";
+        final InputStream inputStream;
+        try {
+            inputStream = Main.class.getResourceAsStream(fileName);
+        } catch (final Exception ex) {
+            throw new RuntimeException(String.format("File \"%s\" not found.", fileName), ex);
+        }
+        if (Objects.isNull(inputStream)) {
+            throw new RuntimeException(String.format("File \"%s\" not found.", fileName));
+        }
+        // Act
+        final List<MnistUtils.MnistData> mnistDataList = MnistUtils.readMnistData(inputStream);
+
+        // Assert
+        assertNotNull(mnistDataList);
+        assertEquals(60000, mnistDataList.size());
+    }
+
+}
---
+++ src/test/de/schmiereck/gnn/demo2/MnistUtils.java
@@ -0,0 +1,53 @@
+package de.schmiereck.gnn.demo2;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 28 x 28 Pixel (256 gray values per pixel)
+ * = 784 Pixels
+ * First Column is the number shown in the picture.
+ *  training set of 60,000 examples
+ *  test set of 10,000 examples
+ */
+public abstract class MnistUtils {
+    public static class MnistData {
+        int number;
+        int[] pixelArr = new int[28 * 28];
+    }
+
+    public static List<MnistData> readMnistData(final InputStream inputStream) {
+        final List<String[]> content = readCsvData(inputStream);
+
+        final List<MnistData> mnistDataList = content.stream().map(strArr -> {
+            final MnistData mnistData = new MnistData();
+            mnistData.number = Integer.parseInt(strArr[0]);
+            for (int pos = 0; pos < mnistData.pixelArr.length; pos++) {
+                mnistData.pixelArr[pos] = Integer.parseInt(strArr[pos + 1]);
+            }
+            return mnistData;
+        }).collect(Collectors.toList());
+
+        return mnistDataList;
+    }
+
+    public static List<String[]> readCsvData(final InputStream inputStream) {
+        final List<String[]> content = new ArrayList<>();
+        try (final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.add(line.split(","));
+            }
+        } catch (final IOException ex) {
+            throw new RuntimeException(String.format("IO error while reading file."), ex);
+        }
+        return content;
+    }
+}
