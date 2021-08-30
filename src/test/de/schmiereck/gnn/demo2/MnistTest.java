d3fc22eb46c23a09ab03c5918dc75415cfea8f8a
schmiereck <thomas@schmiereck.de>
2021-08-30 14:53:27 +0200

    Add Mnist.

--- src/test/de/schmiereck/gnn/demo2/MnistTest.java
+++ src/test/de/schmiereck/gnn/demo2/MnistTest.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import org.junit.jupiter.api.Test;
 
 import de.schmiereck.gnn.Main;
-import de.schmiereck.gnn.MnistUtils;
+import de.schmiereck.gnn.demo2.MnistUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -18,16 +18,17 @@ public class MnistTest {
     @Test
     public void testReadMnistData() {
         // Arrange
-        final String fileName = "./mnist_train.csv";
+        final String fileName = "mnist_train.csv";
         final InputStream inputStream;
         try {
-            inputStream = Main.class.getResourceAsStream(fileName);
+            inputStream = Main.class.getClassLoader().getResourceAsStream(fileName);
         } catch (final Exception ex) {
             throw new RuntimeException(String.format("File \"%s\" not found.", fileName), ex);
         }
         if (Objects.isNull(inputStream)) {
             throw new RuntimeException(String.format("File \"%s\" not found.", fileName));
         }
+
         // Act
         final List<MnistUtils.MnistData> mnistDataList = MnistUtils.readMnistData(inputStream);
 
